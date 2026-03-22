2b753c660c1d5d7f08aa88585ee31ffa3e79438b
Johnny Salazar L <salazarleonjohnny@gmail.com>
2026-03-22 14:01:59 -0600

    este es un nuevo cambio test

---
+++ mi-archivo.py
@@ -0,0 +1,11 @@
+def saludar(nombre):
+    print(f"Hola, {nombre}. ¡Bienvenida al ejercicio de Git y GitHub!")
+def sumar(a, b):
+    resultado = a + b
+    print(f"La suma de {a} + {b} es: {resultado}")
+def main():
+    print("¡Hola, mundo desde Java!")
+    saludar("nombre")
+    sumar(5, 3)
+if __name__ == "__main__":
+    main()
